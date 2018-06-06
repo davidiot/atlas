22cdc47b705f4c72210278bb1fcaad76fbea0dd3
David Zhou <xdavidzhou@gmail.com>
2018-06-06 16:37:55 -0700

    code now classifies and scales

--- code/bounding_boxer.py
+++ code/bounding_boxer.py
@@ -5,25 +5,65 @@ import json
 
 from PIL import Image
 
+# counts: {(10, 10): 4088, (30, 30): 3228, (70, 70): 3049, (150, 150): 1313}
 BOX_SHAPES = [(10, 10), (30, 30), (70, 70), (150, 150)]
 
 
-def compute_bounding_box(input_path, padding, show_image=False):
+def compute_bounding_box(input_path, show_image=False):
     input_image = Image.open(input_path).convert("1")  # convert to black and white
     input_array = np.array(input_image)
     vertical_indices = np.argwhere(input_array.sum(axis=0))[:, 0]
     horizontal_indices = np.argwhere(input_array.sum(axis=1))[:, 0]
+
     x1 = vertical_indices[0]
     x2 = vertical_indices[-1] + 1
     y1 = horizontal_indices[0]
     y2 = horizontal_indices[-1] + 1
-    bbox = (max(x1 - padding, 0),
-            max(y1 - padding, 0),
-            min(x2 + padding, input_array.shape[0] - 1),
-            min(y2 + padding, input_array.shape[1] - 1))
+
+    # Determine the smallest box shape that fits the mask
+    height = x2 - x1
+    width = y2 - y1
+    output_box_shape = input_array.shape
+    for shape in BOX_SHAPES:
+        if width <= shape[0] + 2 and height <= shape[1] + 2:
+            output_box_shape = shape
+            break
+
+    x_center = (x1 + x2) / 2
+    y_center = (y1 + y2) / 2
+
+    # if the edges of the box are outside of the input image, shift it
+    output_x1 = x_center - output_box_shape[0] / 2
+    output_x2 = x_center + output_box_shape[0] / 2
+    output_y1 = y_center - output_box_shape[1] / 2
+    output_y2 = y_center + output_box_shape[1] / 2
+    if output_x1 < 0:
+        diff = 0 - output_x1
+        output_x1 += diff
+        output_x2 += diff
+    if output_y1 < 0:
+        diff = 0 - output_y1
+        output_y1 += diff
+        output_y2 += diff
+    if output_x2 > input_array.shape[0] - 1:
+        diff = output_x2 - input_array.shape[0] + 1
+        output_x1 -= diff
+        output_x2 -= diff
+    if output_y2 > input_array.shape[1] - 1:
+        diff = output_y2 - input_array.shape[1] + 1
+        output_y1 -= diff
+        output_y2 -= diff
+
+    bbox = (output_x1, output_y1, output_x2, output_y2)
+    assert(output_x2 - output_x1 == output_box_shape[0])
+    assert(output_y2 - output_y1 == output_box_shape[1])
+    assert(output_x1 >= 0)
+    assert(output_y1 >= 0)
+    assert(output_x2 <= input_array.shape[0] - 1)
+    assert(output_y2 <= input_array.shape[1] - 1)
     if show_image:
         input_image.crop(bbox).show()
-    return bbox
+    return output_box_shape, bbox
 
 
 def generate_canonical_boxes(FLAGS):
@@ -33,13 +73,8 @@ def generate_canonical_boxes(FLAGS):
     shape_counts = {shape: 0 for shape in BOX_SHAPES}
     for slice_path in mask_slice_paths:
         try:
-            bbox = compute_bounding_box(slice_path, 0)
-            height = bbox[3] - bbox[1]
-            width = bbox[2] - bbox[0]
-            for shape in BOX_SHAPES:
-                if width <= shape[0] + 2 and height<= shape[1] + 2:
-                    shape_counts[shape] = shape_counts[shape] + 1
-                    break
+            shape, bbox = compute_bounding_box(slice_path, True)
+            shape_counts[shape] += 1
             # base_name = os.path.splitext(slice_path)[0]
             # with open(base_name + "-canonical.json", "w") as fout:
             #     json.dump(compute_bounding_box(slice_path, 3, True), fout)
